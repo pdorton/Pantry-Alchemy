0b35539d34786c7678d8803e17ef4dfed9344c21
erodriguez1555 <erodriguez1555@gmail.com>
2017-04-02 18:28:58 -0400

    started writing insertion script for measurements

---
+++ SQL/insertionScript.sql
@@ -0,0 +1,34 @@
+USE pantry_database;
+
+
+
+
+INSERT VALUES into pantry_database.measurements(unit, unit_name)
+VALUES("tsp.","Teaspoon");
+
+INSERT VALUES into pantry_database.measurements(unit, unit_name)
+VALUES("tbsp.","Tablespoon");
+
+INSERT VALUES into pantry_database.measurements(unit, unit_name)
+VALUES("fl. oz.","Fluid Ounce");
+
+INSERT VALUES into pantry_database.measurements(unit, unit_name)
+VALUES("cup","Cup");
+
+INSERT VALUES into pantry_database.measurements(unit, unit_name)
+VALUES("pt.","Pint");
+
+INSERT VALUES into pantry_database.measurements(unit, unit_name)
+VALUES("qt.","Quart");
+
+INSERT VALUES into pantry_database.measurements(unit, unit_name)
+VALUES("gal.","Gallon");
+
+
+
+
+
+
+
+
+
