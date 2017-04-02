95d7290c952119aa32e9b087fc5a2544c48bdf9c
erodriguez10 <erodriguez1555@gmail.com>
2017-04-02 18:50:32 -0400

    Update createDB.sql

--- SQL/createDB.sql
+++ SQL/createDB.sql
@@ -64,7 +64,7 @@ CREATE TABLE pantry_ingredient(
 	quantity INT NOT NULL,
 	CONSTRAINT PK_INGREDIENTS PRIMARY KEY(ing_id, pid),
 	FOREIGN KEY(ing_id) REFERENCES ingredients(id),
-	FOREIGN KEY(measure_id) REFERENCES measurments(id),
+	FOREIGN KEY(measure_id) REFERENCES measurements(id),
 	FOREIGN KEY(pid) REFERENCES pantry(id)
 );
 
@@ -75,6 +75,6 @@ CREATE TABLE recipe_ingredient(
 	quantity INT NOT NULL,
 	CONSTRAINT PK_INGREDIENTS PRIMARY KEY(ing_id, rid),
 	FOREIGN KEY(ing_id) REFERENCES ingredients(id),
-	FOREIGN KEY(measure_id) REFERENCES measurments(id),
+	FOREIGN KEY(measure_id) REFERENCES measurements(id),
 	FOREIGN KEY(rid) REFERENCES recipe(id)
 );
