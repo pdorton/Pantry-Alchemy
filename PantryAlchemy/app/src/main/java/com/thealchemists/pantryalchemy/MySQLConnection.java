3b609597b8f670215bdbe6c2e6d773b848c4a65c
Lara Aydin <laraaydin@knights.ucf.edu>
2017-04-04 19:50:07 -0400

    Establish MySQLConnection file

---
+++ PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/MySQLConnection.java
@@ -0,0 +1,20 @@
+package com.thealchemists.pantryalchemy;
+
+import java.sql.*;
+
+public class MySQLConnection {
+
+    // used to establish the connection with the specified url, username and password
+    public static Connection getConnection(String url,String userName,String password) {
+
+    }
+
+    // creates a statement object that can be used to execute SQL queries
+    public Statement createStatement() {
+
+    }
+
+    public void close() {
+
+    }
+}
