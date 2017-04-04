3c53c0048a79d8f75119b2595010a2f1c007de51
erodriguez10 <erodriguez1555@gmail.com>
2017-04-04 18:44:49 -0400

    Update measurements.sql

--- SQL/measurements.sql
+++ SQL/measurements.sql
@@ -2,31 +2,31 @@ USE pantry_database;
 
 
 
-INSERT VALUES into pantry_database.measurements(unit, unit_name)
+INSERT into pantry_database.measurements(unit, unit_name)
 VALUES("tsp.","Teaspoon");
 
-INSERT VALUES into pantry_database.measurements(unit, unit_name)
+INSERT into pantry_database.measurements(unit, unit_name)
 VALUES("Tbsp.","Tablespoon");
 
-INSERT VALUES into pantry_database.measurements(unit, unit_name)
+INSERT into pantry_database.measurements(unit, unit_name)
 VALUES("fl. oz.","Fluid Ounce");
 
-INSERT VALUES into pantry_database.measurements(unit, unit_name)
+INSERT into pantry_database.measurements(unit, unit_name)
 VALUES("cup","Cup");
 
-INSERT VALUES into pantry_database.measurements(unit, unit_name)
+INSERT into pantry_database.measurements(unit, unit_name)
 VALUES("pt.","Pint");
 
-INSERT VALUES into pantry_database.measurements(unit, unit_name)
+INSERT into pantry_database.measurements(unit, unit_name)
 VALUES("qt.","Quart");
 
-INSERT VALUES into pantry_database.measurements(unit, unit_name)
+INSERT into pantry_database.measurements(unit, unit_name)
 VALUES("gal.","Gallon");
 
-INSERT VALUES into pantry_database.measurements(unit, unit_name)
+INSERT into pantry_database.measurements(unit, unit_name)
 VALUES("g.","Grams");
 
-INSERT VALUES into pantry_database.measurements(unit, unit_name)
+INSERT into pantry_database.measurements(unit, unit_name)
 VALUES("lb.","Pound");
 
 
