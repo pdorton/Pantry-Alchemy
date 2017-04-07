ab585ba43dbefbb29977f10469e17f56077f1260
mfanx2 <marcanthonyfanfan@knights.ucf.edu>
2017-04-06 20:59:49 -0400

    Fixed search bar bug

--- PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/MainActivity.java
+++ PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/MainActivity.java
@@ -1,6 +1,8 @@
 package com.thealchemists.pantryalchemy;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.speech.RecognizerIntent;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +12,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,8 +20,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
+
+import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -62,9 +70,51 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(mViewPager);
 
         searchView = (MaterialSearchView) findViewById(R.id.search_view);
+        searchView.setVoiceSearch(true);
+        final MaterialSearchView searchView = (MaterialSearchView) findViewById(R.id.search_view);
 
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                searchView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                searchView.setVisibility(View.GONE);
+            }
+        });
+
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                toast(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Do some magic
+                return false;
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == RESULT_OK) {
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (matches != null && matches.size() > 0) {
+                String searchWrd = matches.get(0);
+                if (!TextUtils.isEmpty(searchWrd)) {
+                    searchView.setQuery(searchWrd, false);
+                }
+            }
+
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -140,5 +190,10 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
+    }
+
+    // Test method for easy toast messages
+    private void toast(String text) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
     }
 }
--- PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/MySQLConnection.java
+++ PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/MySQLConnection.java
@@ -6,15 +6,17 @@ public class MySQLConnection {
 
     // used to establish the connection with the specified url, username and password
     public static Connection getConnection(String url,String userName,String password) {
-
+        Connection test = null;
+        return test;
     }
 
     // creates a statement object that can be used to execute SQL queries
     public Statement createStatement() {
-
+        Statement test = null;
+        return test;
     }
 
     public void close() {
-
+        return;
     }
 }
