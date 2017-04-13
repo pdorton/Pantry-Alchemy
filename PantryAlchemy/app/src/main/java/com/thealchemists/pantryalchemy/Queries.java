4b3c925f4f2f6b3df1f85328ae4711ad38594533
erodriguez10 <erodriguez1555@gmail.com>
2017-04-13 18:06:56 -0400

    Finished getIngredients

--- PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/Queries.java
+++ PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/Queries.java
@@ -1,4 +1,5 @@
-
+import java.sql.*
+import java.util.ArrayList;
 
 public class Queries {
 
@@ -7,7 +8,25 @@ public class Queries {
     }
 
     protected static String getIngredients() {
+        
+    	ArrayList<String> ingredientList = new ArrayList<String>();
+		MySQLConnection connect = new MySQLConnection();
+		PreparedStatement psmt = null;
 
+		psmt = connect.startConnection().prepareStatement("SELECT ing_name FROM ingredients ORDER BY ing_name");
+
+		ResultSet results = psmt.executeQuery();
+
+		while (results.next()){
+			ingredientList.add(results.getString("ing_name"));
+		}
+			
+		for(int i = 0; i < ingredientList.size(); i++){
+			System.out.println(ingredientList.get(i));
+		}
+        
+		connect.CloseConnection();
+		return ingredientList;
     }
 
     protected static Image getPhoto() {
