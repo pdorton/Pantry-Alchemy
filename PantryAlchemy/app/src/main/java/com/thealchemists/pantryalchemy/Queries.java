6902f193f9d8925fffb49a88103309516ab389ac
erodriguez10 <erodriguez1555@gmail.com>
2017-04-13 18:07:25 -0400

    fixed tabbing

--- PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/Queries.java
+++ PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/Queries.java
@@ -10,23 +10,23 @@ public class Queries {
     protected static String getIngredients() {
         
     	ArrayList<String> ingredientList = new ArrayList<String>();
-		MySQLConnection connect = new MySQLConnection();
-		PreparedStatement psmt = null;
+	MySQLConnection connect = new MySQLConnection();
+	PreparedStatement psmt = null;
 
-		psmt = connect.startConnection().prepareStatement("SELECT ing_name FROM ingredients ORDER BY ing_name");
+	psmt = connect.startConnection().prepareStatement("SELECT ing_name FROM ingredients ORDER BY ing_name");
 
-		ResultSet results = psmt.executeQuery();
+	ResultSet results = psmt.executeQuery();
 
-		while (results.next()){
-			ingredientList.add(results.getString("ing_name"));
-		}
-			
-		for(int i = 0; i < ingredientList.size(); i++){
-			System.out.println(ingredientList.get(i));
-		}
-        
-		connect.CloseConnection();
-		return ingredientList;
+	while (results.next()){
+		ingredientList.add(results.getString("ing_name"));
+	}
+
+	for(int i = 0; i < ingredientList.size(); i++){
+		System.out.println(ingredientList.get(i));
+	}
+
+	connect.CloseConnection();
+	return ingredientList;
     }
 
     protected static Image getPhoto() {
