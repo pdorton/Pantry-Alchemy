6d4c01b8e3f7cf3ee2e69e675575d4b50f26bafa
erodriguez10 <erodriguez1555@gmail.com>
2017-04-13 18:27:01 -0400

    cleaned up code, now this class only makes the connection to the database

--- PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/MySQLConnection.java
+++ PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/MySQLConnection.java
@@ -4,56 +4,17 @@ import java.sql.*;
 
 public class MySQLConnection {
 
-    private static void request() {
-
-        String url = "jdbc:mysql://162.243.207.144:3306/pantry_database";
-        String userName = "user";
-        String password = "password";
-        String query = "select name from pantry_database.ingredients;";
-        Connection conn = null;
-
-        try {
-            /* Initializing the connection */
-            conn = DriverManager.getConnection(url, userName, password);
-
-            Statement statement = conn.createStatement();
-
-            ResultSet resultset = statement.executeQuery(query);
-
-            int columnIndex = 0;
-
-            while(resultset.next() && columnIndex <= resultset.getFetchSize()){
-                System.out.println(resultset.getString(columnIndex));       
-            }
-        }
-        
-        catch(SQLException e){
-            System.out.println("SQL connection error: " + e.getMessage());
-        }
-        
-        finally {
-            if(conn != null) {
-                try {
-                    /* CLosing connection */
-                    conn.close();
-                }
-                
-                catch (SQLException e) {
-                    System.out.println("Error while closing the connection: " + e.getMessage());
-                }
-            }
-        }
+    String url = "jdbc:mysql://162.243.207.144:3306/pantry_database?useSSL=false";
+    String userName = "admin";
+    String password = "Pantry1!";
+    Connection conn = null;
+    
+    
+    public Connection startConnection() throws SQLException{
+			return conn = DriverManager.getConnection(url, userName, password);
     }
-    public static void main(String[] args) {
-        // Loading the MySQL Connector/J driver
-        try { 
-            Class.forName("com.mysql.jdbc.Driver");
-        }
-        
-        catch(ClassNotFoundException e){
-            System.out.println("Error while loading the Driver: " + e.getMessage());
-        }
-
-        request();
-    }
+    
+    public void CloseConnection() throws SQLException{
+		conn.close();
+	}
 }
