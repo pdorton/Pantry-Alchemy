2fef2e26866f862e2933e5d7dce6892afee36255
erodriguez1555 <erodriguez1555@gmail.com>
2017-04-16 21:41:48 -0400

    made methods to grab stuff from DB

--- PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/DataBaseHelper.java
+++ PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/DataBaseHelper.java
@@ -1,6 +1,5 @@
 package com.thealchemists.pantryalchemy;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,11 +11,10 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
 import android.util.Log;
 
 public class DataBaseHelper extends SQLiteOpenHelper{
-
-
 
     //The Android's default system path of your application database.
     private static String DB_PATH = "/data/data/com.thealchemists.pantryalchemy/databases/";
@@ -38,9 +36,18 @@ public class DataBaseHelper extends SQLiteOpenHelper{
         this.myContext = context;
     }
 
+    @Override
+    public void onCreate(SQLiteDatabase db) {
 
+    }
 
-    protected void openDataBase() throws SQLException{
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    //opens a read only DB
+    protected void openDataBaseRead() throws SQLException{
 
         //Open the database
         String myPath = myContext.getDatabasePath(DB_NAME).getPath();
@@ -51,9 +58,18 @@ public class DataBaseHelper extends SQLiteOpenHelper{
 
     }
 
+    //opens DB to be able to be written to
+    protected  void openDataBaseWrite() throws SQLException{
+        //Open the database
+        String myPath = myContext.getDatabasePath(DB_NAME).getPath();
+        if(myDataBase != null && myDataBase.isOpen()){
+            return;
+        }
+        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+    }
 
+    //copies DB to phone on first app startup
     protected static boolean copyDataBase(Context context) {
-
 
         try{
             Log.i("Database", "DB copying initiated");
@@ -77,7 +93,7 @@ public class DataBaseHelper extends SQLiteOpenHelper{
 
     }
 
-
+    //closes db
     public void closeDatabase() {
 
         if(myDataBase != null)
@@ -85,12 +101,13 @@ public class DataBaseHelper extends SQLiteOpenHelper{
 
     }
 
-    public ArrayList<String> getIngredientList(){
+    //makes arraylist of strings of all ingredients in ingredients table
+    protected ArrayList<String> getIngredientList(){
 
         String query = "SELECT ing_name FROM ingredients ORDER BY ing_name";
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<>();
         Log.i("Getting DB", "getting DB");
-        openDataBase();
+        openDataBaseRead();
         Log.i("Getting DB", "Got the DB");
 
         Cursor results = myDataBase.rawQuery(query, null);
@@ -105,18 +122,98 @@ public class DataBaseHelper extends SQLiteOpenHelper{
         closeDatabase();
         return list;
 
+    }
 
 
+    //inserts ingredient
+    protected void insertIngredient(String ingName){
+
+        long i = 0;
+        //open writable db
+        openDataBaseWrite();
+        Log.i("db", "opened writable db");
+        //make insert statement
+        SQLiteStatement stmt = myDataBase.compileStatement("INSERT INTO ingredients (ing_name) VALUES (?)");
+
+        //bind the ing name and execute the statement
+        try{
+            myDataBase.beginTransaction();
+            stmt.bindString(1, ingName);
+            i = stmt.executeInsert();
+            Log.i("dberror", "" + i);
+            myDataBase.setTransactionSuccessful();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            myDataBase.endTransaction();
+        }
+        //close db
+        closeDatabase();
 
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
+    //makes a ingredient arraylist based off of recipeId param
+    protected ArrayList<Ingredient> getRecipesIngredients(int recipeId){
+        ArrayList<Ingredient> ingredients = new ArrayList<>();
+        Ingredient tempIngredient = null;
+        Log.i("get List", "Getting DB");
+        openDataBaseRead();
+        Log.i("get List", "Got DB");
 
+        String query = "SELECT ing_name, unit, quantity "
+                    + "FROM recipe_ingredient "
+                    + "JOIN ingredients, measurements, recipe "
+                    + "WHERE recipe_ingredient.rid = recipe.id "
+                    + "AND recipe_ingredient.measure_id = measurements.id "
+                    + "AND recipe_ingredient.ing_id = ingredients.id "
+                    + "AND recipe.id = "+ recipeId;
+
+        try{
+            Cursor results = myDataBase.rawQuery(query, null);
+            results.moveToFirst();
+            while(!results.isAfterLast()){
+                tempIngredient = new Ingredient(results.getString(0), results.getString(1), results.getInt(2));
+                ingredients.add(tempIngredient);
+                results.moveToNext();
+            }
+            results.close();
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        closeDatabase();
+        return ingredients;
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    //makes a recipe
+    protected Recipe makeRecipe(int recipeId) throws SQLException{
 
+        ArrayList<Ingredient> ingredients = getRecipesIngredients(recipeId);
+        Recipe thisRecipe = null;
+        openDataBaseRead();
+
+        String query = "SELECT recipe_name, directions, photo "
+                    + "FROM recipe "
+                    + "WHERE recipe.id = " + recipeId;
+
+        try{
+            Cursor results = myDataBase.rawQuery(query, null);
+            if(results.moveToFirst()){
+                //get photo as blob
+
+                thisRecipe = new Recipe(results.getString(0), results.getString(1), results.getBlob(2), ingredients);
+                closeDatabase();
+                return thisRecipe;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        closeDatabase();
+        return thisRecipe;
     }
+
+
 }
--- PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/Tab2Pantry.java
+++ PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/Tab2Pantry.java
@@ -1,30 +1,17 @@
 package com.thealchemists.pantryalchemy;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.ContextWrapper;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
-
 import java.io.File;
-import java.sql.*;
-
-
-
-import java.io.IOException;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
+
 
 
 import java.util.ArrayList;
@@ -51,17 +38,25 @@ public class Tab2Pantry extends Activity implements AdapterView.OnItemSelectedLi
         //debug
         //Log.i("get List", "Getting list");
 
-        //get ingredients
-        ArrayList<String> ingredientList = myDbHelper.getIngredientList();
-        for(int i =0;i <ingredientList.size()-1;i++){
-            Log.i("List", ingredientList.get(i));
-        }
+        //this makes recipe call to make recipe
+        //Recipe recipe = myDbHelper.makeRecipe(1);
+
+        //debug
+        //Log.i("start", "start printing list");
+
+        //debug
+        //Log.i("recipe", recipe.getRecipe_name());
+
+
+        //get ingredients for spinner
+        ArrayList<String> ingredientList = new ArrayList<>();
+        ingredientList = myDbHelper.getIngredientList();
 
         //debug
         //Log.i("checking", "all ingredients there if there are " +ingredientList.size() );
 
 
-        /*FloatingActionButton addToPantry = (FloatingActionButton) findViewById(R.id.pantryAddIngredientButton);
+        FloatingActionButton addToPantry = (FloatingActionButton) findViewById(R.id.pantryAddIngredientButton);
         final Spinner pantryIngredientListSpinner = (Spinner) findViewById(R.id.ingredientAdderSpinner);
 
         //TODO:DATABASE: NEED TO PUT CODE HERE TO INSERT ALL THE INGREDIENTS FROM THE SQL CALL
@@ -72,16 +67,16 @@ public class Tab2Pantry extends Activity implements AdapterView.OnItemSelectedLi
         final EditText pantryIngredientList = (EditText) findViewById(R.id.pantryIngredientDisplay);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         pantryIngredientListSpinner.setAdapter(dataAdapter);
-/*
 
-        pantryIngredientListSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        /*pantryIngredientListSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 int selectionPos = pantryIngredientListSpinner.getSelectedItemPosition();
                 String ingredientToAddToPantry = (String) pantryIngredientListSpinner.getItemAtPosition(selectionPos);
                 pantryIngredientList.append(ingredientToAddToPantry);
             }
-        });
+        });*/
 
         addToPantry.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,7 +86,7 @@ public class Tab2Pantry extends Activity implements AdapterView.OnItemSelectedLi
                 pantryIngredientListSpinner.setVisibility(View.VISIBLE);
 
             }
-        });*/
+        });
 
 
 
