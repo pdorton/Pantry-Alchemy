c53faa2713cb4aaf8bed5b8b60e00fe5d8ddecd8
Kevin <kevinvii.exe@gmail.com>
2017-04-18 12:43:01 -0400

    Adds onResume method to MainActivity

--- PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/MainActivity.java
+++ PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/MainActivity.java
@@ -3,15 +3,28 @@ package com.thealchemists.pantryalchemy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.thealchemists.pantryalchemy.R;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
+
+
+    private LoginButton loginButton;
+    private AccessToken accessToken;
+    private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
+
     CallbackManager callbackManager;
 
 
@@ -23,10 +36,13 @@ public class MainActivity extends FragmentActivity {
 
         LoginManager.getInstance().registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
-                    @Override
 
+                    // Grabs profile and switches activity upon successful login
+                    @Override
                     public void onSuccess(LoginResult loginResult) {
-                        launchActivity();
+                        AccessToken accessToken = loginResult.getAccessToken();
+                        Profile profile = Profile.getCurrentProfile();
+                        launchActivity(profile);
                     }
 
                     @Override
@@ -41,9 +57,25 @@ public class MainActivity extends FragmentActivity {
                 });
     }
 
-    private void launchActivity(){
-        Intent recipeIntent = new Intent(MainActivity.this, Tab3Recipes.class);
-        startActivity(recipeIntent);
+    // When reopening the app and still logged in
+    // bypasses login page to given activity
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Profile profile = Profile.getCurrentProfile();
+        launchActivity(profile);
+    }
+
+
+    // Checks if user is logged in
+    // Launches next Activity to be displayed
+    // TODO: Set method to take in type int and pass to Intent array
+    private void launchActivity(Profile profile){
+        if (profile != null) {
+            Intent recipeIntent = new Intent(MainActivity.this, Tab3Recipes.class);
+            startActivity(recipeIntent);
+        }
+
     }
 
     @Override
