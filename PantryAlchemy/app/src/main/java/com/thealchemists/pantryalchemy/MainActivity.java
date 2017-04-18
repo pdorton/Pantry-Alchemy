4fadf98aecf44e78bf93cdd7c11e0b22ca39a8fa
Kevin <kevinvii.exe@gmail.com>
2017-04-17 20:52:28 -0400

    Progress to switch upon Successful login

--- PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/MainActivity.java
+++ PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.media.Image;
 import android.speech.RecognizerIntent;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.support.v4.app.Fragment;
@@ -14,61 +15,184 @@ import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 import java.util.ArrayList;
 
+
+
 public class MainActivity extends AppCompatActivity {
     private MaterialSearchView searchView;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private AccessToken accessToken;
+    private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
 
 
+    private FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            Profile profile = Profile.getCurrentProfile();
+            nextActivity(profile);
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+
+        @Override
+        public void onError(FacebookException error) {
+
+        }
+    };
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+            }
+        };
+
+        profileTracker = new ProfileTracker(){
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile){
+                nextActivity(newProfile);
+            }
+        };
+
+        accessTokenTracker.startTracking();
+        profileTracker.startTracking();
+
+
+        LoginButton loginButton = (LoginButton)findViewById(R.id.fb_login_bn);
+        callback = new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
+                nextActivity(profile);
+                Toast.makeText(getApplicationContext(), "Logging in...", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        };
+        loginButton.setReadPermissions("user_friends");
+        loginButton.registerCallback(callbackManager, callback);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.d("CREATION", "onCreate: MAINACTIVITY ");
-        // Init super.onCreate and set initial content view
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onResume(){
+        super.onResume();
+        Profile profile = Profile.getCurrentProfile();
+        nextActivity(profile);
+    }
 
-        // Create and configure toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Pantry Alchemy");
-        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    protected void onStop(){
+        super.onStop();
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void nextActivity(Profile profile){
+        if(profile != null){
+            Intent main = new Intent(MainActivity.this, Tab2Pantry.class );
+            startActivity(main);
+        }
+    }
 
 
 
-        // Set up search view
-        searchView = (MaterialSearchView) findViewById(R.id.search_view);
-        searchView.setVoiceSearch(true);
-        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
-            @Override
-            public void onSearchViewShown() {
-                searchView.setVisibility(View.VISIBLE);
-            }
 
-            @Override
-            public void onSearchViewClosed() {
-                searchView.setVisibility(View.GONE);
-            }
-        });
-        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                toast("You searched for: " + query);
-                return false;
-            }
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //Do some magic
-                return false;
-            }
-        });
+        //Log.d("CREATION", "onCreate: MAINACTIVITY ");
+
+
+
+
+
+
+
+
+//        // Create and configure toolbar
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setTitle("Pantry Alchemy");
+//        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+//
+//
+//
+//        // Set up search view
+//        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+//        searchView.setVoiceSearch(true);
+//        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+//            @Override
+//            public void onSearchViewShown() {
+//                searchView.setVisibility(View.VISIBLE);
+//            }
+//
+//            @Override
+//            public void onSearchViewClosed() {
+//                searchView.setVisibility(View.GONE);
+//            }
+//        });
+//        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                toast("You searched for: " + query);
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                //Do some magic
+//                return false;
+//            }
+//        });
 
 //        Button pantryButton = (Button)findViewById(R.id.mainPantryButton);
 //        pantryButton.setOnClickListener(new View.OnClickListener() {
@@ -83,21 +207,29 @@ public class MainActivity extends AppCompatActivity {
 //                startActivity(pantryIntent);
 //            }
 //        });
-    }
+//    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == RESULT_OK) {
-            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            if(matches != null && matches.size() > 0) {
-                String searchWrd = matches.get(0);
-                if(!TextUtils.isEmpty(searchWrd)) searchView.setQuery(searchWrd, false);
-            }
 
-            return;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
+
+
+
+
+
+
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if(requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == RESULT_OK) {
+//            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//            if(matches != null && matches.size() > 0) {
+//                String searchWrd = matches.get(0);
+//                if(!TextUtils.isEmpty(searchWrd)) searchView.setQuery(searchWrd, false);
+//            }
+//
+//            return;
+//        }
+//        super.onActivityResult(requestCode, resultCode, data);
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -107,21 +239,16 @@ public class MainActivity extends AppCompatActivity {
         searchView.setMenuItem(item);
         return true;
     }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        if(id == R.id.action_settings) {
+//            toast("Settings are a work in progress...");
+//            return true;
+//        }
+//        else return super.onOptionsItemSelected(item);
+//    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if(id == R.id.action_settings) {
-            toast("Settings are a work in progress...");
-            return true;
-        }
-        else return super.onOptionsItemSelected(item);
-    }
-
-
-
-
-    // Debugging
-    private void toast(String text) { Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show(); }
 }
