74131aebda3768b15b302a1b8b63c89e8319ebe7
Kevin <kevinvii.exe@gmail.com>
2017-04-19 14:32:01 -0400

    Adds FB login to PatricksBranch

--- PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/MainActivity.java
+++ PantryAlchemy/app/src/main/java/com/thealchemists/pantryalchemy/MainActivity.java
@@ -1,127 +1,85 @@
 package com.thealchemists.pantryalchemy;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.media.Image;
-import android.speech.RecognizerIntent;
-import android.support.design.widget.TabLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-import com.miguelcatalan.materialsearchview.MaterialSearchView;
-import java.util.ArrayList;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.thealchemists.pantryalchemy.R;
 
 public class MainActivity extends AppCompatActivity {
-    private MaterialSearchView searchView;
 
+
+    private LoginButton loginButton;
+    private AccessToken accessToken;
+    private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
+
+    CallbackManager callbackManager;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.d("CREATION", "onCreate: MAINACTIVITY ");
-        // Init super.onCreate and set initial content view
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        callbackManager = CallbackManager.Factory.create();
 
-        // Create and configure toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Pantry Alchemy");
-        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+
+                    // Grabs profile and switches activity upon successful login
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        //AccessToken accessToken = loginResult.getAccessToken();
+                        Profile profile = Profile.getCurrentProfile();
+                        launchActivity(profile);
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+    }
+
+    // When reopening the app and still logged in
+    // bypasses login page to given activity
+//    @Override
+//    protected void onResume(){
+//        super.onResume();
+//        Profile profile = Profile.getCurrentProfile();
+//        launchActivity(profile);
+//    }
 
 
+    // Checks if user is logged in
+    // Launches next Activity to be displayed
+    // TODO: Set method to take in type int and pass to Intent array
+    private void launchActivity(Profile profile){
 
-        // Set up search view
-        searchView = (MaterialSearchView) findViewById(R.id.search_view);
-        searchView.setVoiceSearch(true);
-        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
-            @Override
-            public void onSearchViewShown() {
-                searchView.setVisibility(View.VISIBLE);
-            }
+        Intent recipeIntent = new Intent(MainActivity.this, Tab3Recipes.class);
+        startActivity(recipeIntent);
 
-            @Override
-            public void onSearchViewClosed() {
-                searchView.setVisibility(View.GONE);
-            }
-        });
-        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                toast("You searched for: " + query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //Do some magic
-                return false;
-            }
-        });
-
-        Button pantryButton = (Button)findViewById(R.id.mainPantryButton);
-        pantryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                Intent[] intents = new Intent[3];
-
-                Intent pantryIntent = new Intent(getApplicationContext(),Tab2Pantry.class);
-
-                intents[0] = pantryIntent;
-                startActivity(pantryIntent);
-            }
-        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == RESULT_OK) {
-            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            if(matches != null && matches.size() > 0) {
-                String searchWrd = matches.get(0);
-                if(!TextUtils.isEmpty(searchWrd)) searchView.setQuery(searchWrd, false);
-            }
-
-            return;
-        }
         super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        MenuItem item = menu.findItem(R.id.action_search);
-        searchView.setMenuItem(item);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if(id == R.id.action_settings) {
-            toast("Settings are a work in progress...");
-            return true;
-        }
-        else return super.onOptionsItemSelected(item);
-    }
-
-
-
-
-    // Debugging
-    private void toast(String text) { Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show(); }
 }
